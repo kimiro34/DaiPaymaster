921fc9d17a53e8f20d46dcfdb4f060c3ddc842ea
Kimiro34 <ermoshinvladislav34@gmail.com>
2021-12-27 11:01:07 +0800

    add dai paymaster

---
+++ DaiPaymaster.ts
@@ -0,0 +1,369 @@
+import { ethers, BigNumberish } from "ethers";
+import { keccak256, defaultAbiCoder, solidityPack } from "ethers/lib/utils.js";
+import { ecsign } from "ethereumjs-util";
+
+const getPermitDigest = (
+  approve: {
+    owner: string;
+    spender: string;
+    value: BigNumberish;
+  },
+  nonce: BigNumberish,
+  deadline: BigNumberish,
+  allowed: Boolean
+) => {
+  const DOMAIN_SEPARATOR =
+    "0x16c71d5e724b4895d1eda159ed52657b98b9383418cdf9872697474064bbc728";
+  const PERMIT_TYPEHASH =
+    "0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb";
+  return keccak256(
+    solidityPack(
+      ["bytes1", "bytes1", "bytes32", "bytes32"],
+      [
+        "0x19",
+        "0x01",
+        DOMAIN_SEPARATOR,
+        keccak256(
+          defaultAbiCoder.encode(
+            ["bytes32", "address", "address", "uint256", "uint256", "bool"],
+            [
+              PERMIT_TYPEHASH,
+              approve.owner,
+              approve.spender,
+              nonce,
+              deadline,
+              allowed,
+            ]
+          )
+        ),
+      ]
+    )
+  );
+};
+
+export const sign = (digest: any, privateKey: any) => {
+  return ecsign(Buffer.from(digest.slice(2), "hex"), privateKey);
+};
+
+const daiAbi = [
+  {
+    inputs: [{ internalType: "uint256", name: "chainId_", type: "uint256" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "src", type: "address" },
+      { indexed: true, internalType: "address", name: "guy", type: "address" },
+      { indexed: false, internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: true,
+    inputs: [
+      { indexed: true, internalType: "bytes4", name: "sig", type: "bytes4" },
+      { indexed: true, internalType: "address", name: "usr", type: "address" },
+      { indexed: true, internalType: "bytes32", name: "arg1", type: "bytes32" },
+      { indexed: true, internalType: "bytes32", name: "arg2", type: "bytes32" },
+      { indexed: false, internalType: "bytes", name: "data", type: "bytes" },
+    ],
+    name: "LogNote",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "src", type: "address" },
+      { indexed: true, internalType: "address", name: "dst", type: "address" },
+      { indexed: false, internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "DOMAIN_SEPARATOR",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "PERMIT_TYPEHASH",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "burn",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [{ internalType: "address", name: "guy", type: "address" }],
+    name: "deny",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "mint",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "src", type: "address" },
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "move",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "nonces",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "holder", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "nonce", type: "uint256" },
+      { internalType: "uint256", name: "expiry", type: "uint256" },
+      { internalType: "bool", name: "allowed", type: "bool" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "permit",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "pull",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "push",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [{ internalType: "address", name: "guy", type: "address" }],
+    name: "rely",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "src", type: "address" },
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "version",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "wards",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const daiAddress = "0x307b3c3ebfba36a0c1828e0208c22c42a5380acc";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+  "rinkeby"
+);
+const signer = new ethers.Wallet(
+  "1a56162ba22ceeb49dc178415f16051f2e9bf49b66c9425db9e8b0641694fdb3",
+  provider
+);
+const chainId = 4;
+// The Contract object
+const daiContract = new ethers.Contract(daiAddress, daiAbi, provider);
+const daiWithSigner = daiContract.connect(signer);
+const ownerPrivateKey = Buffer.from(
+  "7759e563c7ce2ae8935ada616395ddaf8c51d39cf860dfee7b04b9c95544aedb",
+  "hex"
+);
+var deadline = 100000000000000;
+const allowed = true;
+const approve = {
+  owner: "0xEeDC7C9824878cf672b6f0691097B9241e683e1e",
+  spender: "0x8231FF1b0c3Ae734DA58E8F38F1212d9AcbfCBB9",
+  value: 100,
+};
+
+const contractName = await daiContract.name();
+const nonce = await daiContract.nonces(approve.owner);
+const permitDigest = getPermitDigest(approve, nonce, deadline, allowed);
+const { v, r, s } = sign(permitDigest, ownerPrivateKey);
+
+await daiWithSigner.permit(
+  approve.owner,
+  approve.spender,
+  nonce,
+  deadline,
+  allowed,
+  v,
+  r,
+  s
+);
+
+// const dai = ethers.utils.parseUnits("1.0", 18);
+
+await daiWithSigner.transferFrom(approve.owner, approve.spender, 1);
